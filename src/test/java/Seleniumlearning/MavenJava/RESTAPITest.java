32169648e53ab2f3e7ea2c3509f578df71e9d2d8
raghu <ragha23@gmail.com>
2021-11-04 16:45:09 +0000

    Commit from Gitstuff

--- src/test/java/Seleniumlearning/MavenJava/RESTAPITest.java
+++ src/test/java/Seleniumlearning/MavenJava/RESTAPITest.java
@@ -16,6 +16,8 @@ public class RESTAPITest {
 	public void secondRestAPiTest()	
 	{
 		System.out.println("This is Second RESTAPI Test");
+		System.out.println("Testing GIT and GITHUB");
+		System.out.println("GIT Commands");
 	}
 
 }
